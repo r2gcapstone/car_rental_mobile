c3709a14053879dd42401890c304767a0efccb28
Yusri <yusripawaki2@gmail.com>
2023-07-17 16:50:04 +0800

    chore(tests): add jest as unit testing framework

--- App.js
+++ App.js
@@ -2,11 +2,6 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  // console.log("App executed");
-
-  // let x;
-  // x.toString();
-
   return (
     <View style={styles.container}>
       <Text>Hello World! dd</Text>
