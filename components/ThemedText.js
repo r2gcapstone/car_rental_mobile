448ceedcfeafe672b0f17807b1a0875464d54e90
Yusri <yusripawaki2@gmail.com>
2023-07-26 13:18:30 +0800

    SCR-117 feat: add default theme components

---
+++ components/ThemedText.js
@@ -0,0 +1,16 @@
+import React, { useContext } from "react";
+import { Text } from "react-native";
+import ThemeContext from "../context/ThemeContext";
+
+export default function ThemedText(props) {
+  const theme = useContext(ThemeContext);
+
+  const { style, ...otherProps } = props;
+  const textStyle = [
+    { fontFamily: theme.fontFamily },
+    { color: theme.textColor },
+    style,
+  ];
+
+  return <Text style={textStyle} {...otherProps} />;
+}
