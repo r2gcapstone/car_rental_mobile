ba9ea497bb3ed000c2de6cb09f0b957f7bb0cf80
Yusri <yusripawaki2@gmail.com>
2023-08-24 11:49:33 +0800

    SCR-42 chore: handle fields value

--- app/(tabs)/(rent-a-vehicle)/rent-a-vehicle.js
+++ app/(tabs)/(rent-a-vehicle)/rent-a-vehicle.js
@@ -1,5 +1,7 @@
 import { StyleSheet, Image, ScrollView, TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import { searchAvailableCars } from "api/search";
 
 // Components
 import View from "components/ThemedView";
@@ -8,36 +10,118 @@ import Header from "components/home/Header";
 import RentDateAndTime from "components/rent_a_vehicle/RentDateAndTime";
 import VehicleDropdown from "components/rent_a_vehicle/VehicleType";
 import GearShiftDropdown from "components/rent_a_vehicle/GearType";
+import FuelTypeDropdown from "components/rent_a_vehicle/FuelType";
 
 import { colors } from "constants/Colors";
+import PassengerCount from "components/rent_a_vehicle/PassengerCount";
+import BaggageNumber from "components/rent_a_vehicle/BaggageNumber";
+import PriceRate from "components/rent_a_vehicle/PriceRate";
+
+const initialDateTimeValues = {
+  startRentDate: new Date(),
+  startRentTime: new Date(),
+  endRentDate: new Date(),
+  endRentTime: new Date(),
+};
 
 export default function RentAVehicle() {
+  const [dateTimeValues, setDateTimeValues] = useState(initialDateTimeValues);
+  const [vehicleType, setVehicleType] = useState("default");
+  const [gearType, setGearType] = useState("default");
+  const [fuelType, setFuelType] = useState("default");
+  const [passengerNum, setPassengerNum] = useState("default");
+  const [baggageNum, setBaggageNum] = useState("default");
+  const [priceRate, setPriceRate] = useState("default");
+  const [location, setLocation] = useState("default");
+
+  useEffect(() => {
+    const search = {
+      dateTimeValues: dateTimeValues,
+      filter: {
+        vehicleType: vehicleType,
+        gearType: gearType,
+        fuelType: fuelType,
+        passengerNum: passengerNum,
+        baggageNum: baggageNum,
+        priceRate: priceRate,
+        location: location,
+      },
+    };
+
+    // console.log(search);
+  }, [
+    dateTimeValues,
+    vehicleType,
+    gearType,
+    fuelType,
+    passengerNum,
+    baggageNum,
+    priceRate,
+  ]);
+
+  const handleSearch = async () => {
+    const result = await searchAvailableCars();
+
+    console.log("Search Result:", result);
+  };
+
   return (
     <View style={styles.container}>
       <ScrollView style={styles.scroll} showsVerticalScrollIndicator={false}>
         <Header />
-        <View style={styles.row}>
-          <RentDateAndTime />
+        <View style={styles.row1Container}>
+          <RentDateAndTime
+            dateTimeValues={dateTimeValues}
+            setDateTimeValues={setDateTimeValues}
+          />
           <View
-            style={{ width: "100%", height: 1, backgroundColor: "#fff" }}
+            style={{
+              width: "100%",
+              height: 1,
+              backgroundColor: "#fff",
+              marginTop: -6,
+            }}
           ></View>
-          <Text style={styles.filterText}>Filter (Optional)</Text>
-          <VehicleDropdown />
-          <GearShiftDropdown />
-          <View style={styles.rowData}></View>
-          <View style={styles.rowData}></View>
+
+          <Text style={styles.filterText}>Filter ( Optional )</Text>
+
+          <View style={styles.rowField}>
+            <VehicleDropdown
+              vehicleType={vehicleType}
+              setVehicleType={setVehicleType}
+            />
+          </View>
+          <View style={styles.rowField}>
+            <GearShiftDropdown gearType={gearType} setGearType={setGearType} />
+          </View>
+
+          <View style={styles.rowField}>
+            <FuelTypeDropdown fuelType={fuelType} setFuelType={setFuelType} />
+            <PassengerCount setPassengerNum={setPassengerNum} />
+          </View>
+          <View style={styles.rowField}>
+            <BaggageNumber setBaggageNum={setBaggageNum} />
+            <PriceRate setPriceRate={setPriceRate} />
+          </View>
           <View
-            style={{ width: "100%", height: 1, backgroundColor: "#fff" }}
+            style={{
+              width: "100%",
+              height: 1,
+              backgroundColor: "#fff",
+            }}
           ></View>
           <Text>
             Note : The location of your device will be used to find vehicles
             near you. (Required)
           </Text>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleSearch()}
+          >
             <Text style={styles.buttonText}>Search</Text>
           </TouchableOpacity>
         </View>
-        <View style={styles.row2}>
+        <View style={styles.row2Container}>
           <View style={styles.col}>
             <Image
               style={styles.icon}
@@ -67,17 +151,17 @@ const styles = StyleSheet.create({
     flex: 1,
     height: "100%",
   },
-  row: {
+  row1Container: {
     flex: 1,
     marginBottom: 15,
     flexDirection: "column",
-    gap: 23,
+    gap: 10,
     padding: 17,
     borderRadius: 10,
     overflow: "hidden",
     backgroundColor: colors.blue.slitedark,
   },
-  row2: {
+  row2Container: {
     marginBottom: 15,
     alignItems: "center",
     justifyContent: "space-between",
@@ -88,11 +172,12 @@ const styles = StyleSheet.create({
     backgroundColor: colors.blue.slitedark,
     flexDirection: "row",
   },
-  rowData: {
+  rowField: {
     flex: 1,
-    backgroundColor: "#fff",
-    height: 10,
-    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: colors.blue.slitedark,
+    gap: 10,
   },
   icon: {
     width: 32,
@@ -131,7 +216,10 @@ const styles = StyleSheet.create({
   },
   filterText: {
     alignSelf: "flex-start",
-    fontSize: 14,
-    fontWeight: "bold",
+    fontSize: 16,
+    fontWeight: "500",
+  },
+  label: {
+    marginBottom: -15,
   },
 });
