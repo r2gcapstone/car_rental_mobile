479665effa7e3e2ddf8ec2b2859e811c1c9b040d
Yusri <yusripawaki2@gmail.com>
2023-08-29 16:55:21 +0800

    change page name

---
+++ app/(tabs)/rent-my-vehicle/index.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function RentMyVehicle() {
+  return (
+    <View>
+      <Text>rent-a-vehicle</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
