2442441ba44efaadf0a970317f1fed4cc9456bfc
Yusri <yusripawaki2@gmail.com>
2023-09-15 12:35:28 +0800

    fix: result screen and refactor some of component

--- api/search.js
+++ api/search.js
@@ -1,26 +1,36 @@
-import { getDocs, collection, query, where } from "firebase/firestore";
+// Import necessary functions from Firebase Firestore and Auth
+import {
+  getDocs,
+  collection,
+  query,
+  where,
+  getDoc,
+  doc,
+} from "firebase/firestore";
 import { db } from "../services/firebaseConfig";
+import { getAuth } from "@firebase/auth";
 
+// Define the searchAvailableCars function
 export const searchAvailableCars = async ({ filter }) => {
   try {
+    // Store the filter options
     const filterOptions = filter;
-    // console.log("filter:", filterOptions);
 
+    // Get a reference to the 'cars' collection
     const carsRef = collection(db, "cars");
     let queryRef = query(carsRef);
 
-    // Define an array of filters and their corresponding fields
+    // Define the filters and their corresponding fields in the database
     const filters = [
-      { key: "vehicleType", field: "type" },
-      { key: "gearType", field: "gearShift" },
+      { key: "vehicleType", field: "vehicleType" },
+      { key: "gearType", field: "gearType" },
       { key: "fuelType", field: "fuelType" },
       { key: "passengerNum", field: "passengerCount", parseToInt: true },
       { key: "baggageNum", field: "luggageCount", parseToInt: true },
       { key: "priceRate", field: "priceRate", parseToInt: true },
-      // Add more filters here based on your requirements
     ];
 
-    // Apply filters based on the filterOptions object
+    // Apply the filters to the query
     filters.forEach(({ key, field, parseToInt }) => {
       if (filterOptions[key] !== "") {
         const value = parseToInt ? +filterOptions[key] : filterOptions[key];
@@ -31,18 +41,35 @@ export const searchAvailableCars = async ({ filter }) => {
     // Execute the query
     const querySnapshot = await getDocs(queryRef);
 
-    const availableCars = [];
-    querySnapshot.forEach((doc) => {
-      const carData = doc.data();
-      // Include additional filtering based on dateTimeValues if needed
-      // ...
+    if (!Array.isArray(querySnapshot.docs)) {
+      console.error("Error: querySnapshot.docs is not an array");
+      return;
+    }
+    // For each document in the querySnapshot, get the owner's name and add it to the car data
+    // For each document in the querySnapshot, get the owner's name and add it to the car data
+    const availableCarsPromises = querySnapshot.docs.map(async (carDoc) => {
+      const carData = carDoc.data();
 
-      availableCars.push({
-        id: doc.id,
+      // Get the owner's document from the 'users' collection
+      const ownerDoc = await getDoc(doc(db, "users", carData.userId));
+
+      // Get the owner's data
+      const ownerData = ownerDoc.data();
+
+      // Add the owner's name to the car data
+      carData.ownerName = ownerData.firstName;
+
+      // Return the car data
+      return {
+        id: carDoc.id,
         ...carData,
-      });
+      };
     });
 
+    // Wait for all promises to resolve
+    const availableCars = await Promise.all(availableCarsPromises);
+
+    // If no cars were found, return a message indicating this
     if (!availableCars.length) {
       return {
         message: "No available cars found!",
@@ -50,8 +77,11 @@ export const searchAvailableCars = async ({ filter }) => {
       };
     }
 
+    // Return the available cars
+    console.log(JSON.stringify(availableCars, null, 2));
     return { searchResults: availableCars };
   } catch (error) {
+    // If an error occurred, log it and return an error message
     console.error("Error searching available cars:", error);
     return { error: true, message: error.message, status: error.code };
   }
