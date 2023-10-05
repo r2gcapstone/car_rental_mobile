d0194f9e78a93158771429856476473023a21454
Yusri <yusripawaki2@gmail.com>
2023-10-05 16:23:32 +0800

    feat: add rmv application for renting screen

--- api/rental.js
+++ api/rental.js
@@ -1,6 +1,14 @@
-import { db } from "../services/firebaseConfig";
-import { doc, deleteDoc } from "firebase/firestore";
+import { db, auth } from "../services/firebaseConfig";
+import {
+  doc,
+  deleteDoc,
+  collection,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
 
+//Delete user rental request
 export const deleteRentRequest = async (docId) => {
   try {
     // get the document reference
@@ -10,6 +18,36 @@ export const deleteRentRequest = async (docId) => {
     await deleteDoc(docRef);
 
     return { status: "success", message: "Document successfully deleted!" };
+  } catch (error) {
+    return { status: "error", message: error.message };
+  }
+};
+
+//Get rental request based on userId
+export const getAllRentals = async () => {
+  try {
+    const user = auth.currentUser;
+    const userId = user.uid;
+
+    // get rentals reference
+    const collectionRef = collection(db, "rentals");
+
+    // Create a query against the collection
+    const q = query(collectionRef, where("ownerId", "==", userId));
+
+    // get all rentals
+    const rentalSnapshot = await getDocs(q);
+
+    let rentals = [];
+    rentalSnapshot.docs.forEach((doc) => {
+      const rental = doc.data();
+
+      rentals.push(rental);
+    });
+
+    // console.log(JSON.stringify(rentals, null, 2));
+
+    return rentals;
   } catch (error) {
     return { status: "error", message: error.message };
   }
