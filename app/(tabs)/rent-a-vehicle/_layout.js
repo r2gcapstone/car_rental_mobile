b06ac5a5643aded30eaff083085a24157305ffbc
Yusri <yusripawaki2@gmail.com>
2023-10-19 16:08:22 +0800

    style: set for emulator view

--- app/(tabs)/rent-a-vehicle/_layout.js
+++ app/(tabs)/rent-a-vehicle/_layout.js
@@ -72,6 +72,8 @@ const styles = StyleSheet.create({
 const CustomBackButton = ({ customText, router }) => (
   <TouchableOpacity
     style={{
+      marginTop: 40, // emulator
+      paddingBottom: 10, // emulator
       flexDirection: "row",
       alignItems: "center",
       gap: 8,
